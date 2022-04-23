3771ad8457493bb0cdcc458e5a014bb3e683c4cc
JohnnyLucky <roninpirat@gmail.com>
2022-04-23 11:06:48 +0900

    new

--- src/com/company/learning/lesson4.java
+++ src/com/company/learning/lesson4.java
@@ -3,6 +3,10 @@ package com.company.learning;
 public class lesson4 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        int x = 2;
+        int y = 3;
+        int z = x + y;
+        System.out.println(z);
 
     }
 }
