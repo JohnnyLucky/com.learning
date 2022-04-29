4defcbc1c12b96befcb91b8ae4b7e62bb7ef6de9
JohnnyLucky <roninpirat@gmail.com>
2022-04-30 00:40:46 +0900

    30.04.22

---
+++ src/com/company/task/task1.java
@@ -0,0 +1,16 @@
+package com.company.task;
+
+public class task1 {
+    public static void main(String[] args) {
+        int [] sum = new int[7];
+        for (int i = 0; i < sum.length; i++) {
+            sum[0] = 10;
+            sum[1] = 20;
+            sum[2] = 30;
+
+            System.out.println(sum[i]);
+        }
+    }
+}
+
+
